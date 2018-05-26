f8ec507002182dbf723ca650916621595607d91f
Hadi Yousufi <reach.hadiy@gmail.com>
2018-05-26 14:59:44 -0500

    adds product details to page

--- app.js
+++ app.js
@@ -5,7 +5,22 @@ var app = new Vue({
   data: {
     product: 'Nike Epic React Flyknit',
     image: './img/whitej.jpg',
-    link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=Nike+Epic+React+Flyknit&rh=i%3Aaps%2Ck%3ANike+Epic+React+Flyknit'
-  } 
-})
-
+    category: 'Running',
+    link:
+      'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=Nike+Epic+React+Flyknit&rh=i%3Aaps%2Ck%3ANike+Epic+React+Flyknit',
+    onSale: true,
+    inStock: true,
+    details: ['Flexible Knit Encloure', 'Sole Cusioned', 'Style: AQ0067-300'],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: 'green'
+      },
+      {
+        variantId: 2235,
+        variantColor: 'blue'
+      }
+    ],
+    sizes: [7, 8, 10, 10.5, 11, 11.5, 12]
+  }
+});
